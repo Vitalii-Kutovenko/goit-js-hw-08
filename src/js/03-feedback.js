b93c3c1f25fa4fc759498a58f8e5757cc59239a0
Vitalii Kutovenko <idorikk@gmail.com>
2023-04-06 13:27:31 +0300

    5

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(e => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500));
+window.addEventListener('load', () => {
+    const savedState = localStorage.getItem('feedback-form-state');
+    if (savedState) {
+      const { email, message } = JSON.parse(savedState);
+      emailInput.value = email;
+      messageInput.value = message;
+    }
+  });
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  });
